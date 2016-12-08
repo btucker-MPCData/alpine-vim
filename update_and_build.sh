15764e9690d4a01663da8e22586ef852e5043cdf
Ben Tucker <benjamint@bsquare.com>
2016-12-08 12:59:45 +0000

    Top level build script builds all sub containers
    Change-Id: Ifc74e2b56e2793fa8cb6318e1b5f84191ac4c745
    Signed-off-by: Ben Tucker <benjamint@bsquare.com>

--- update_and_build.sh
+++ update_and_build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+cd alpine-vim-base
+./build.sh
+
+cd ../vim-pathogen
+./build.sh
+
+cd ../wrapper
+./build.sh
+
+cd ..
+
 git submodule update --init --recursive
 git submodule update --remote --merge
 docker build -t benjamint/vim-bundle .
